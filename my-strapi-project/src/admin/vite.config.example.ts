be3b0c88e3aa35fe469f31e0bcea484d2a8578de
Ankith M <ankithankith022@gmail.com>
2024-09-06 21:54:21 +0530

    #

--- my-strapi-project/src/admin/vite.config.example.ts
+++ my-strapi-project/src/admin/vite.config.example.ts
@@ -1,6 +1,6 @@
 const { mergeConfig } = require('vite');
 
-module.exports = (config) => {
+module.exports = (config:any) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
