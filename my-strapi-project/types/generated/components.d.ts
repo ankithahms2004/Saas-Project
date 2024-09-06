cf7cd652d6b4e3a17bbc6ef5dbf86a43358cde2d
Ankith M <ankithankith022@gmail.com>
2024-09-07 00:42:06 +0530

    #

--- my-strapi-project/types/generated/components.d.ts
+++ my-strapi-project/types/generated/components.d.ts
@@ -11,6 +11,39 @@ export interface SkillsSkills extends Struct.ComponentSchema {
   };
 }
 
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+    icon: 'clock';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    workSummery: Schema.Attribute.Text;
+  };
+}
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'stack';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+  };
+}
+
 export interface SharedSlider extends Struct.ComponentSchema {
   collectionName: 'components_shared_sliders';
   info: {
@@ -73,50 +106,17 @@ export interface SharedMedia extends Struct.ComponentSchema {
   };
 }
 
-export interface EducationEducation extends Struct.ComponentSchema {
-  collectionName: 'components_education_educations';
-  info: {
-    displayName: 'Education';
-    icon: 'stack';
-  };
-  attributes: {
-    universityName: Schema.Attribute.String;
-    degree: Schema.Attribute.String;
-    major: Schema.Attribute.String;
-    startDate: Schema.Attribute.String;
-    endDate: Schema.Attribute.String;
-    description: Schema.Attribute.Text;
-  };
-}
-
-export interface ExperienceExperience extends Struct.ComponentSchema {
-  collectionName: 'components_experience_experiences';
-  info: {
-    displayName: 'Experience';
-    icon: 'clock';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    companyName: Schema.Attribute.String;
-    city: Schema.Attribute.String;
-    state: Schema.Attribute.String;
-    startDate: Schema.Attribute.String;
-    endDate: Schema.Attribute.String;
-    workSummery: Schema.Attribute.Text;
-  };
-}
-
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'skills.skills': SkillsSkills;
+      'experience.experience': ExperienceExperience;
+      'education.education': EducationEducation;
       'shared.slider': SharedSlider;
       'shared.seo': SharedSeo;
       'shared.rich-text': SharedRichText;
       'shared.quote': SharedQuote;
       'shared.media': SharedMedia;
-      'education.education': EducationEducation;
-      'experience.experience': ExperienceExperience;
     }
   }
 }
