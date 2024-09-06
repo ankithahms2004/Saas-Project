40f599a96e7e20a95a7eb03e26df1e2befd1f467
Ankith M <ankithankith022@gmail.com>
2024-09-07 00:44:15 +0530

    #

--- my-strapi-project/types/generated/components.d.ts
+++ my-strapi-project/types/generated/components.d.ts
@@ -28,22 +28,6 @@ export interface ExperienceExperience extends Struct.ComponentSchema {
   };
 }
 
-export interface EducationEducation extends Struct.ComponentSchema {
-  collectionName: 'components_education_educations';
-  info: {
-    displayName: 'Education';
-    icon: 'stack';
-  };
-  attributes: {
-    universityName: Schema.Attribute.String;
-    degree: Schema.Attribute.String;
-    major: Schema.Attribute.String;
-    startDate: Schema.Attribute.String;
-    endDate: Schema.Attribute.String;
-    description: Schema.Attribute.Text;
-  };
-}
-
 export interface SharedSlider extends Struct.ComponentSchema {
   collectionName: 'components_shared_sliders';
   info: {
@@ -106,17 +90,33 @@ export interface SharedMedia extends Struct.ComponentSchema {
   };
 }
 
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'stack';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'skills.skills': SkillsSkills;
       'experience.experience': ExperienceExperience;
-      'education.education': EducationEducation;
       'shared.slider': SharedSlider;
       'shared.seo': SharedSeo;
       'shared.rich-text': SharedRichText;
       'shared.quote': SharedQuote;
       'shared.media': SharedMedia;
+      'education.education': EducationEducation;
     }
   }
 }
